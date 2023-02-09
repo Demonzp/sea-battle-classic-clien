2bee61930da5eeaebe1105aeeefff0a26837463f
DemoN <demon.ua.zp@gmail.com>
2023-02-09 09:33:44 +0200

    update

--- layouts/Layout/Layout.tsx
+++ layouts/Layout/Layout.tsx
@@ -6,14 +6,29 @@ type Props = {
   children: JSX.Element
 }
 
-const Layoute:React.FC<Props> = ({children})=>{
+const Layoute: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </>
+      <div className='videoWrapper'>
+        <video
+          className="bgVideod"
+          autoPlay
+          loop={true}
+          muted
+        >
+          <source
+            src="./background-video.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
 
+      <div className="mainWraper">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
 
   );
 };
